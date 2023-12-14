ad2f9b09fa2b0ddeaa09b3ac1dcba94957a7a005
Tyler Lang <tylerlang94@protonmail.com>
2023-12-14 10:43:02 -0700

    Browser Bookmarks
    Started adding logic for the bookmarks

--- BrowserBackup/Program.cs
+++ BrowserBackup/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Win32;
 using System.Diagnostics;
+using Serilog;
+
 
 namespace ConsoleApp3
 {
@@ -97,7 +99,7 @@ namespace ConsoleApp3
 			if (File.Exists(oneDrivePath))
 			{
 				isInstalled = true;
-				
+
 			}
 
 			return isInstalled;
@@ -112,6 +114,88 @@ namespace ConsoleApp3
 			var oneDrivePath = Environment.GetEnvironmentVariable("OneDrive");
 
 			return oneDrivePath;
+		}
+
+		/// <summary>
+		/// Gets the bookmarks for the specified browser
+		/// </summary>
+		public void GetBookmarks()
+		{
+			BrowserApplication defaultBrowser = GetDefaultBrowser();
+			switch (defaultBrowser)
+			{
+				case BrowserApplication.Edge:
+					break;
+				case BrowserApplication.InternetExplorer:
+					break;
+				case BrowserApplication.Firefox:
+					break;
+				case BrowserApplication.Chrome:
+					break;
+				case BrowserApplication.Opera:
+					break;
+				default:
+					break;
+			}
+		}
+
+		public void EdgeBookmarks()
+		{
+			BrowserApplication defaultBrowser = GetDefaultBrowser();
+
+			if (defaultBrowser == BrowserApplication.Edge)
+			{
+
+			}
+		}
+
+		public void InternetExplorerBookmarks()
+		{
+			BrowserApplication defaultBrowser = GetDefaultBrowser();
+
+			if (defaultBrowser == BrowserApplication.InternetExplorer)
+			{
+
+			}
+		}
+
+		public void FirefoxBookmarks()
+		{
+			BrowserApplication defaultBrowser = GetDefaultBrowser();
+
+			if (defaultBrowser == BrowserApplication.Firefox)
+			{
+
+			}
+		}
+		public void ChromeBookmarks()
+		{
+			BrowserApplication defaultBrowser = GetDefaultBrowser();
+
+			if (defaultBrowser == BrowserApplication.Chrome)
+			{
+
+			}
+		}
+
+		public void OperaBookmarks()
+		{
+			BrowserApplication defaultBrowser = GetDefaultBrowser();
+
+			if (defaultBrowser == BrowserApplication.Opera)
+			{
+
+			}
+		}
+
+		public void SafariBookmarks()
+		{
+			BrowserApplication defaultBrowser = GetDefaultBrowser();
+
+			if (defaultBrowser == BrowserApplication.Safari)
+			{
+
+			}
 		}
 	}
 }
